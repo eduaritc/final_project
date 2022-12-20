bfd7c209d991c6859f5842d5a5a0ee30120d38c8
Eduar Mancera <eduarecnam@gmail.com>
2022-12-20 19:09:58 +0000

    hbase schema completed V1

--- info_manager.py
+++ info_manager.py
@@ -5,28 +5,26 @@ import pandas as pd
 cf = "review_info"
 hbase_table = {
     "table":{"namespace":"default", "name":"amazon_reviews"}, \
-    "rowkey":"key", \
-    "columns": {}
     }
 columns = []
 
 amazon_reviews_df = pd.read_csv("amazon_reviews.csv")
+
 for index, row in amazon_reviews_df.iterrows():
-    columns.append({
-        "rowkey":index, \
-        "col"+str(index):{"cf":cf, "colour":row["colour"]}, \
-        "col"+str(index):{"cf":cf, "country":row["country"]}, \
-        "col"+str(index):{"cf":cf, "date":row["date"]}, \
-        "col"+str(index):{"cf":cf, "price":row["price"]}, \
-        "col"+str(index):{"cf":cf, "size":row["size"]}, \
-        "col"+str(index):{"cf":cf, "stars":row["stars"]}, \
-        "col"+str(index):{"cf":cf, "text":row["text"]}, \
-        "col"+str(index):{"cf":cf, "title":row["title"]}
-    })
-hbase_table["columns"] = columns
+    hbase_table[index] = {
+                "col0":{"cf":cf, "colour":row["colour"]}, \
+                "col1":{"cf":cf, "country":row["country"]}, \
+                "col2":{"cf":cf, "date":row["date"]}, \
+                "col3":{"cf":cf, "price":row["price"]}, \
+                "col4":{"cf":cf, "size":row["size"]}, \
+                "col5":{"cf":cf, "stars":row["stars"]}, \
+                "col6":{"cf":cf, "text":row["text"]}, \
+                "col7":{"cf":cf, "title":row["title"]} \
+                }
+# print(hbase_table)
 amazon_reviews_pandas_df = pd.DataFrame(hbase_table)
-amazon_reviews_df.to_csv("amazon_reviews_hbase.csv")
-print(hbase_table)
+amazon_reviews_pandas_df.to_csv("amazon_reviews_hbase.csv")
+# print(hbase_table)
 
 
 
